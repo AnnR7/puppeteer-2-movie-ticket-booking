340c59fa215ab67ba1edb1feaed2b29cc63ca789
Anna Russkikh <nyusha82@inbox.ru>
2024-01-30 22:37:44 +0300

    Change commit

--- features/search.feature
+++ features/search.feature
@@ -2,21 +2,25 @@ Feature: Movie-ticket booking
     Scenario: One ticket happy test 
         Given user on page
         When user selects date
+        When user selects time
         When user selects place
         When user booking ticket
         Then user sees button 'Получить код подтверждения'
 
-    # Scenario: Two vip ticket happy test
-    #     Given user on page
-    #     When user selects date
-    #     When user selects first vip place
-    #     When user selects second vip place
-    #     When user booking ticket
-    #     Then user sees button 'Получить код подтверждения'
+    Scenario: Two vip ticket happy test
+        Given user on page
+        When user selects date
+        When user selects time
+        When user selects first vip place
+        When user selects second vip place
+        When user booking ticket
+        Then user sees button 'Получить код подтверждения'
 
-    # Scenario: Two ticket sad test
-    #     Given 
-    #     When 
-    #     Then 
-
+    Scenario: Two ticket sad test
+        Given user on page
+        When user selects date
+        When user selects time 
+        When user selects first busy place
+        When user selects second busy place 
+        Then user sees text 'Выберите билет'
         
--- features/step_definitions/search.steps.js
+++ features/step_definitions/search.steps.js
@@ -3,6 +3,9 @@ const chai = require("chai");
 const expect = chai.expect;
 const { Given, When, Then, Before, After} = require("cucumber");
 const { putText, getText, clickElement } = require("../../lib/commands.js");
+const {setDefaultTimeout} = require("cucumber");
+
+setDefaultTimeout(60 * 1000);
 
 Before(async function () {
   const browser = await puppeteer.launch({ headless: false, slowMo: 50 });
@@ -18,46 +21,45 @@ After(async function () {
 });
 
 Given("user on page", async function () {
-  return await this.page.goto("https://qamid.tmweb.ru/client/index.php", {
-    setTimeout: 100000,
-  });
+  return await this.page.goto("https://qamid.tmweb.ru/client/index.php")
 });
 
 When("user selects date", async function () {
-  return await  clickElement(this.page, "body > nav > a:nth-child(6)", {
-    setTimeout: 100000,
-  });
+  return await  clickElement(this.page, "body > nav > a:nth-child(6)");
 });
 
 When("user selects time", async function () {
-  return await clickElement(this.page, "body > main > section:nth-child(1) > div.movie-seances__hall > ul > li:nth-child(3) > a", {
-    setTimeout: 100000,
-  });
+  return await clickElement(this.page, "body > main > section:nth-child(1) > div.movie-seances__hall > ul > li:nth-child(3) > a") 
 });
 
 When("user selects place", async function () {
-  return await this.page.click(this.page, "body > main > section > div.buying-scheme > div.buying-scheme__wrapper > div:nth-child(3) > span:nth-child(5)", {
-    setTimeout: 100000,
-  });
+  return await clickElement(this.page, "body > main > section > div.buying-scheme > div.buying-scheme__wrapper > div:nth-child(3) > span:nth-child(5)")
 });
 
-// When("user selects first vip place", async function () {
-//   return await this.page.click(this.page, "body > main > section > div.buying-scheme > div.buying-scheme__wrapper > div:nth-child(4) > span.buying-scheme__chair.buying-scheme__chair_vip", {
-//     setTimeout: 100000,
-//   });
-// });
+When("user selects first vip place", async function () {
+  return await clickElement(this.page, "body > main > section > div.buying-scheme > div.buying-scheme__wrapper > div:nth-child(4) > span.buying-scheme__chair.buying-scheme__chair_vip")
+});
 
-// When("user selects second vip place", async function () {
-//   return await clickElement(this.page, "body > main > section > div.buying-scheme > div.buying-scheme__wrapper > div:nth-child(5) > span.buying-scheme__chair.buying-scheme__chair_vip", {
-//     setTimeout: 100000,
-//   });
-// });
+When("user selects second vip place", async function () {
+  return await clickElement(this.page, "body > main > section > div.buying-scheme > div.buying-scheme__wrapper > div:nth-child(5) > span.buying-scheme__chair.buying-scheme__chair_vip")
+});
+
+When("user selects first busy place", async function () {
+  return await clickElement(this.page, "body > main > section > div.buying-scheme > div.buying-scheme__wrapper > div:nth-child(5) > span:nth-child(6)")
+});
+
+When("user selects second busy place", async function () {
+  return await clickElement(this.page, "body > main > section > div.buying-scheme > div.buying-scheme__wrapper > div:nth-child(5) > span:nth-child(8)")
+});
 
 When("user booking ticket", async function () {
-  return await clickElement(this.page, "body > main > section > button", {
-    setTimeout: 100000,
-  });
+  return await clickElement(this.page, "body > main > section > button")
 });
+
+Then("user sees text 'Выберите билет'", async function () {
+  const button = await getText(this.page, "body > main > section > button");
+  await expect(button).toContain("Выберите билет");
+  });  
 
 Then("user sees button 'Получить код подтверждения'", async function () {
   const title3 = ".acceptin-button";
